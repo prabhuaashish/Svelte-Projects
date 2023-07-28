647aeab1f4f37f269cb6bff4f9792d5a7d8d40d3
prabhuaashish <prabhuaashish@gmail.com>
2023-07-28 14:57:52 +0530

    topbar

---
+++ spotify-proj/src/lib/components/Navigation.svelte
@@ -0,0 +1,96 @@
+<script>
+    import {Home, Search, ListMusic} from "lucide-svelte"
+    import logo from "$lib/assets/logo.png";
+    import {page} from "$app/stores";
+	import { SvelteComponent } from "svelte";
+    export let desktop;
+
+    const menuItems = [
+        {path: "/", label: "Home", icon: Home},
+        {path: "/search", label: "Search", icon: Search},
+        {path: "/playlists", label: "Playlists", icon: ListMusic},
+    ]
+</script>
+
+<div class="nav-content" class:desktop class:mobile={!desktop}>
+    <nav aria-label = "Main">
+        <div class="nav-content-inner">
+            <img src={logo} class="logo" alt="Spotify"/>
+            <ul>
+                {#each menuItems as item}
+                    <li class:active={item.path === $page.url.pathname}>
+                        <a href={item.path}>
+                            <svelte:component 
+                            this={item.icon} 
+                            focusable={false}
+                            aria-hidden="true" 
+                            color="var(--text-color)" 
+                            size={26}
+                            strokeWidth={1}/>
+                            {item.label}
+                        </a>
+                    </li>
+                {/each}
+            </ul>
+        </div>
+
+    </nav>
+</div>
+
+<style lang="scss">
+    .nav-content {
+		.logo {
+			max-width: 100%;
+			width: 130px;
+		}
+		.nav-content-inner {
+			padding: 20px;
+			min-width: var(--sidebar-width);
+			background-color: var(--sidebar-color);
+			height: 100vh;
+			overflow: auto;
+			display: none;
+			ul {
+				padding: 0;
+				margin: 20px 0 0;
+				list-style: none;
+				li {
+					&.active {
+						a {
+							opacity: 1;
+						}
+					}
+					a {
+						display: flex;
+						align-items: center;
+						text-decoration: none;
+						color: var(--text-color);
+						font-size: functions.toRem(14);
+						font-weight: 500;
+						padding: 5px;
+						margin: 10px 0;
+						opacity: 0.7;
+						transition: opacity 0.2s;
+						&:hover,
+						&:focus {
+							opacity: 1;
+						}
+						:global(svg) {
+							margin-right: 12px;
+						}
+					}
+				}
+			}
+		}
+		&.desktop {
+			position: sticky;
+			top: 0;
+			.nav-content-inner {
+				@include breakpoint.up('md') {
+					display: block;
+				}
+			}
+		}
+	}
+
+</style>
