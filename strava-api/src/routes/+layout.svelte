a104301e18e2da7f25fdd26e381227b328f98e48
prabhuaashish <prabhuaashish@gmail.com>
2023-08-16 16:30:53 +0530

    getting tokens

---
+++ strava-api/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import "modern-normalize/modern-normalize.css";
+    import "../styles/main.scss";
+    import LogoutButton from "$lib/components/LogoutButton.svelte";
+
+    export let data;
+    $: user = data.user;
+</script>
+
+{#if user}
+	<p>Hello {user.firstname}</p>
+	<LogoutButton />
+{/if}
+
+<slot><!-- optional fallback --></slot>
